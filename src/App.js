13e4f8b2279e19939c12afee49481ca8050ab32d
riyaz7788 <132046163+riyaz7788@users.noreply.github.com>
2023-05-31 14:18:17 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import LandingPage from './Components/LandingPage';
+import SmileMeter from './Components/SmileMeter';
+
+function App() {
+  return (
+    <div className="">
+      <LandingPage/>
+      {/* <SmileMeter/> */}
+
+    </div>
+  );
+}
+
+export default App;
